3e92c8313757fd47e16a64c5d11d5a331c37fad2
Leandro Gonçalves <leandrogoncalvespr@hotmail.com>
2023-12-09 23:26:58 -0300

    feat: update alert

--- app/mock/api/establishment/[guid].ts
+++ app/mock/api/establishment/[guid].ts
@@ -3,9 +3,10 @@ export default function establishmentHandler() {
     200,
     {
       uuid: "123123",
-      alert: "stringaaaa",
+      alert:
+        "Frete Gratis para compras acima de - 200 reais  Regiao SUDESTE  e 250 DEMAIS regioes!",
       description: "string",
-      phone: "19995755514",
+      phone: "5519991824852",
       installments: 10,
       createdAt: new Date(),
       updatedAt: new Date(),
