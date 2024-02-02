bce1cb0042eb96d47f089b1797996918487ebc18
Leandro Gonçalves <leandrogoncalvespr@hotmail.com>
2024-02-02 08:09:00 -0300

    feat: aaa

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith("/api")) {
     const [_, route] = request.nextUrl.pathname.split("/api");
     return NextResponse.rewrite(
-      new URL(`http://149.102.249.204:3333${route}`).toString()
+      new URL(`http://149.102.249.204:3333${route}`, request.url).toString()
     );
   }
 }
