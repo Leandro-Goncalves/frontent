2506c6a8d8710199b3de518da82b23240d47a9c9
Leandro Gonçalves <leandrogoncalvespr@hotmail.com>
2024-06-02 16:52:28 -0300

    feat: add logs

--- pages/api/image/[id].ts
+++ pages/api/image/[id].ts
@@ -9,10 +9,15 @@ export default async function handler(
   res: NextApiResponse<ResponseData>
 ) {
   const { id } = req.query;
+  console.log("id", id);
   const image = await fetch(`https://cacau.b-cdn.net/${id}`);
+  console.log("image", image);
 
   const blob = await image.blob();
+  console.log("blob", blob);
+
   const buffer = await blob.arrayBuffer();
+  console.log("buffer", buffer);
 
   res.setHeader("Content-Type", blob.type);
   res.setHeader("Content-Disposition", `attachment; filename=${id}`);
